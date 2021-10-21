9662d1a2d2e4b9e04440a5244db496950f44d17f
SUKHDEEP SINGH <sukhdeepsingh0409@gmail.com>
2021-10-21 21:17:56 +0530

    firebase setup

---
+++ app/src/main/java/com/example/pushnotifications/MyFirebaseMessagingService.kt
@@ -0,0 +1,10 @@
+package com.example.pushnotifications
+
+import com.google.firebase.messaging.FirebaseMessagingService
+
+class MyFirebaseMessagingService: FirebaseMessagingService() {
+
+    //generate the notification
+    //attach the notification created with the custom layout
+    //show the notification
+}
